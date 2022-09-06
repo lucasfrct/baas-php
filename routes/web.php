7c9e896c2ca8c45b3672f13be33fc0835b7f8b14
lucasfrct <lucasfrct@gmail.com>
2022-09-06 20:17:44 -0300

    add bootstrap

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\BankAccountController;
 |
 */
 
-Route::get('/', function () { return view('home'); });
+Route::get('/', function () { return view('index'); });
 
 Route::get('/login', function () { return view('login'); });
 
