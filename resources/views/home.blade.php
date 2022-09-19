c474eca4afd026265ad49a52865662bf4231384f
lucasfrct <lucasfrct@gmail.com>
2022-09-19 07:42:18 -0300

    noramalizando o MYSQL

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
 
@@ -9,13 +9,14 @@
     <meta name="theme-color" content="#712cf9">
 
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/bootstrap-5.2.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/bootstrap-icons-1.9.1.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/product.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/main.css') }}" />
     
-    <script src="{{ URL::asset('js/bootstrap-5.1.min.js') }}" defer></script>
-    <script src="{{ URL::asset('js/popper-2.11.min.js') }}" defer></script>
+    <script type="module" src="{{ URL::asset('js/bootstrap-5.1.min.js') }}" defer></script>
+    <script type="module" src="{{ URL::asset('js/popper-2.11.min.js') }}" defer></script>
     
-    <title>Product example · Bootstrap v5.2</title>
+    <title>Baas PHP</title>
 
 </head>
 
